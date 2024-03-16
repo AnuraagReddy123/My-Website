46a30a18092b478174b420728c67c04157d69a8a
Anuraag Reddy <anuraagreddy74@outlook.com>
2024-03-16 01:28:20 -0400

    Basic Home page done

--- mywebsite/website/views.py
+++ mywebsite/website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def projects(request):
+    return render(request, 'projects.html', {})
